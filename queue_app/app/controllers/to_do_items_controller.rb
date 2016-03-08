4a1d08788ba29e76a6fea5a1e7598d4ca1159159
Allie Sudholt <allison.sudholt@gmail.com>
2016-03-08 17:35:25 -0600

    fixed some broke shit

--- queue_app/app/controllers/to_do_items_controller.rb
+++ queue_app/app/controllers/to_do_items_controller.rb
@@ -1,18 +1,22 @@
 class ToDoItemsController < ApplicationController
 
+#require "cgi"
 
   def new
 
-    @todo= ToDoItem.new
+    @todo = ToDoItem.new
+    #@id = params[:list_id]
 
   end
 
   def create
 
     @todo = ToDoItem.new
-    @todo.item = params["todo"]["item"]
-    @todo.list_id = params["todo"]["list_id"]
+    @todo.item = params["to_do_item"][:item]
+    @todo.list_id = params["to_do_item"][:list_id]
     @todo.save
+
+    #@id = cgi.list_id
 
     redirect_to :back
 
